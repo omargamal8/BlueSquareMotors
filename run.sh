b9ffb445af090834e4759ca780a357e09da3060d
Omar Hammad <omargamalh@hotmail.com>
2019-02-06 09:54:02 +0000

    fixed hello world.. next:sizing components

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-sudo docker run -it --name BlueSqMs --rm -v $PWD:/blue_square_motors node
+sudo docker run -it --net=host --name BlueSqMs --rm -v $PWD:/blue_square_motors node
 sudo docker build -t node -f Dockerfile .
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import React, { Component } from 'react';
 
-// import AppBar from '@material-ui/core/AppBar';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography';
+
+class BsmHeader extends React.Component{
+  constructor(props){
+    super(props);
+	    this.state = {
+	      componentSize: {width: "50" ,height: props.height}
+	    }
+ 	 }
+	render(){
+		return (
+	      <AppBar position="relative" style="{">
+	        <Toolbar>
+	          <Typography variant="h6" color="inherit">
+	            News
+	          </Typography>
+	        </Toolbar>
+	      </AppBar>
+	      )
+	}
+}
 
 
-
-ReactDOM.render(<h1>helo</h1>, document.getElementById("root"));
+ReactDOM.render(<BsmHeader/>, document.getElementById("root"));
